0d1b4981ebe9688f45d2f0f32f428a5623fc39da
Marcus Ronelius <marcus.ronelius@gmail.com>
2015-09-06 21:26:04 +0200

    Small fixes

--- mme1/DetailViewController.swift
+++ mme1/DetailViewController.swift
@@ -19,7 +19,8 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         var editButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Edit, target: self, action: "editMeme")
-        navigationItem.rightBarButtonItem = editButton
+        var deleteButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Trash, target: self, action: "deleteMeme")
+        navigationItem.rightBarButtonItems = [editButton, deleteButton]
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -46,6 +47,29 @@ class DetailViewController: UIViewController {
         
         presentViewController(createMemeViewController, animated: true, completion: nil)
     }
+    
+    func deleteMeme() {
+        var meme = (UIApplication.sharedApplication().delegate as! AppDelegate).memes[index]
+        confirmDelete(meme, atIndex: index)
+    }
+    
+    func confirmDelete(meme: Meme, atIndex: NSInteger) {
+        let alert = UIAlertController(title: "Delete Meme", message: "Are you sure you want to delete this meme?", preferredStyle: .ActionSheet)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .Destructive) { (alert: UIAlertAction!) -> Void in
+            // Remove the meme from the array and go back to previous viewcontroller
+            (UIApplication.sharedApplication().delegate as! AppDelegate).memes.removeAtIndex(self.index)
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
